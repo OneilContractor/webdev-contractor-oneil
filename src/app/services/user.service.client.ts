05090fe1aed57000cbeac957f0b84253787479fb
oneilc <contractor.o@husky.neu.edu>
2017-10-10 04:12:24 -0400

    First Intermediate Commit

---
+++ src/app/services/user.service.client.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/Rx';
+import {environment} from '../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable()
+
+export class UserService {
+
+  constructor() {}
+
+  users: [
+    {_id: '123', username: 'alice',    password: 'alice',    firstName: 'Alice',  lastName: 'Wonder'  },
+    {_id: '234', username: 'bob',      password: 'bob',      firstName: 'Bob',    lastName: 'Marley'  },
+    {_id: '345', username: 'charly',   password: 'charly',   firstName: 'Charly', lastName: 'Garcia'  },
+    {_id: '456', username: 'jannunzi', password: 'jannunzi', firstName: 'Jose',   lastName: 'Annunzi' }
+    ];
+
+  api = {
+    'createUser': this.createUser,
+    'findUserById': this.findUserById
+  };
+
+  createUser(user: any) {
+    user._id = Math.random().toString();
+    this.users.push(user);
+    return user;
+  }
+
+  findUserById(userId: String) {
+    return this.users.find(function (user) {
+      return user._id === userId;
+    });
+  }
+
+  findUserByUsername(username: String) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x].username === username) {
+        return this.users[x];
+      }
+    }
+  }
+
+  updateUser(userId, user) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        this.users[x].firstName = user.firstName;
+        this.users[x].lastName = user.lastName;
+        this.users[x].username = user.userName;
+        this.users[x].password = user.password;
+        return this.users[x];
+      }
+    }
+  }
+
+  deleteUser(userId) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        delete this.users[x];
+      }
+    }
+  }
+
+
+}
+
